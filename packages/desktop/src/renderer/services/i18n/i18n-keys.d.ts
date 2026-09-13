20a0212adb9a9f22caaa2ab8709f3676e7d8e71c
Eduardo Jose Costa <59846713+EduCosta85@users.noreply.github.com>
2026-09-13 09:12:00 -0300

    feat(ui): show conversation/team name in the window title

--- packages/desktop/src/renderer/services/i18n/i18n-keys.d.ts
+++ packages/desktop/src/renderer/services/i18n/i18n-keys.d.ts
@@ -281,6 +281,7 @@ export type I18nKey =
   | 'common.delete'
   | 'common.deleteFailed'
   | 'common.deleteSuccess'
+  | 'common.documentTitleWithName'
   | 'common.download'
   | 'common.edit'
   | 'common.error'
